d583ae09de068fb85bee54e1cae7dd524ff8f9fb
Lukáš Juránek <lukyjuranek@centrum.cz>
2018-07-26 14:34:01 +0200

    Button moved to file-navigation panel, so it is shown also in another repos, not just in your personal

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
-const button = document.createElement("button");
+let button = document.createElement("a");
 button.innerHTML = "RawGit";
-button.className = "btn btn-sm float-right";
+button.className = "btn btn-sm new-pull-request-btn";
 button.id = "button";
+
+let div = document.querySelector(".file-navigation.in-mid-page.d-flex.flex-items-start");
+div.appendChild(button);
 
 button.addEventListener("click",function(){
     let oldurl = window.location.href;
@@ -9,12 +12,3 @@ button.addEventListener("click",function(){
     var win = window.open(newurl, '_blank');
     win.focus();
 });
-
-const child = document.querySelector(".edit-link");
-
-const div = document.createElement("div");
-div.className = "edit-link";
-div.appendChild(button);
-
-child.before(div);
-
